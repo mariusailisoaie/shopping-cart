4cd929541289d4a36459f7a2fc910a55c5cdffb9
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2021-01-21 22:39:45 +0100

    Add shopping cart functionality

---
+++ src/components/Cart/Cart.tsx
@@ -0,0 +1,29 @@
+import CartItem from '../CartItem/CartItem'
+// Styles
+import { Wrapper } from './Cart.styles'
+
+import { CartItemType } from '../../App'
+
+type Props = {
+  cartItems: CartItemType[],
+  addToCart: (clickedItem: CartItemType) => void,
+  removeFromCart: (id: number) => void,
+}
+
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+  const calculateTotalPrice = (items: CartItemType[]) =>
+    items.reduce((acc: number, item) => acc + item.amount * item.price, 0)
+
+  return (
+    <Wrapper>
+      <h2>Your shopping cart</h2>
+      {cartItems.length === 0 ? <p>No items in cart.</p> : null}
+
+      {cartItems.map(item => <CartItem key={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart} />)}
+
+      <h2>Total: ${calculateTotalPrice(cartItems).toFixed(2)}</h2>
+    </Wrapper>
+  )
+}
+
+export default Cart
